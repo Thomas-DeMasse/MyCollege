de3576b619b4425783ff5c90e4951c58c235124d
Thomas DeMasse <tjdemasse17@gmail.com>
2023-10-14 09:03:57 -0400

    Delete GUII 2/FinApp - Personal Finance Tracking Application/GUI_8_10 directory

--- GUII 2/FinApp - Personal Finance Tracking Application/GUI_8_10/public/goals/js/goals.js
+++
@@ -1,9 +0,0 @@
-function removeallaccordiotitems() {
-  // description: remove all the accordion items in the screen
-  // will be useful if date picker changes
-  $("#accordionExample").empty();
-}
-
-const goalNameInput = document.getElementById("goalName");
-const goalAmountInput = document.getElementById("goalAmount");
-const goalDateInput = document.getElementById("goalDate");
--- GUII 2/FinApp - Personal Finance Tracking Application/GUI_8_10/public/settings/js/settings.js
+++
@@ -1,9 +0,0 @@
-var fullNameInputButton = document.getElementById('full-name');
-var emailInputButton = document.getElementById('email');
-var ageInputButton = document.getElementById('age');
-var incomeInputButton = document.getElementById('income');
-var saveChangesButton = document.getElementById('save-changes');
-
-var settingsglobalvar = null;
-
-
